ee9e1aae1b0a133b74d3dd18df17c8ef068f552c
vishalj0501 <92500255+vishalj0501@users.noreply.github.com>
2022-05-26 17:09:51 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,8 @@
+import imp
+from operator import mod
+import streamlit as st
+from predict_page import main
+from explore_page import modeltrain
+st.sidebar.selectbox("Explore or Predict",("Explore","Predict"))
+#modeltrain()
+main()
